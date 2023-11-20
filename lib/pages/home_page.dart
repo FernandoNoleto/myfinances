4539f17b0799232dd06e9e204ea5236caad90a3b
fernandonoleto <fernando.barroso.noleto@gmail.com>
2023-11-20 18:31:35 -0300

    feat(home_page): Renderiza um CircularChart como exemplo

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/cupertino.dart';
+import 'package:myfinances/entities/tag.dart';
 import 'package:myfinances/pages/new_expense_page.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 // import 'package:syncfusion_flutter_charts/sparkcharts.dart';
 
+//Entities
+import 'package:myfinances/entities/expense.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -26,9 +29,11 @@ class HomePageState extends StatefulWidget {
 class _HomePageStateState extends State<HomePageState> {
   late List<_ChartData> data;
   late TooltipBehavior _tooltip;
+  // late List<Expense> listExpenses;
 
   @override
   void initState() {
+    // listExpenses = getListExpenses();
     data = [
       _ChartData('David', 25),
       _ChartData('Steve', 38),
@@ -39,8 +44,10 @@ class _HomePageStateState extends State<HomePageState> {
     super.initState();
   }
 
-  List getListExpenses(){
-    return [];
+  Future<List<Expense>> getListExpenses() async{
+    List<Expense> listExpenses = [];
+    listExpenses.add(Expense(name: 'name', value: 10, tag: Tag(color: 1000000, name: 'name')));
+    return listExpenses;
   }
 
   @override
@@ -62,20 +69,25 @@ class _HomePageStateState extends State<HomePageState> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                SfCircularChart(
-                  tooltipBehavior: _tooltip,
-                  title: ChartTitle(text: 'Meus gastos'),
-                  legend: const Legend(isVisible: true, overflowMode: LegendItemOverflowMode.scroll),
-                  series: <CircularSeries<_ChartData, String>>[
-                    DoughnutSeries<_ChartData, String>(
-                      dataSource: data,
-                      explode: true,
-                      xValueMapper: (_ChartData data, _) => data.x, //key: string
-                      yValueMapper: (_ChartData data, _) => data.y, //value: int
-                      dataLabelSettings: const DataLabelSettings(isVisible: true),
-                      name: 'Gold'
-                    ),
-                  ],
+                FutureBuilder(
+                  future: getListExpenses(),
+                  builder: (BuildContext context, snapshot) {
+                    return SfCircularChart(
+                      tooltipBehavior: _tooltip,
+                      title: ChartTitle(text: 'Meus gastos'),
+                      legend: const Legend(isVisible: true, overflowMode: LegendItemOverflowMode.scroll),
+                      series: <CircularSeries<_ChartData, String>>[
+                        DoughnutSeries<_ChartData, String>(
+                            dataSource: data,
+                            explode: true,
+                            xValueMapper: (_ChartData data, _) => data.x, //key: string
+                            yValueMapper: (_ChartData data, _) => data.y, //value: int
+                            dataLabelSettings: const DataLabelSettings(isVisible: true),
+                            name: 'Gold'
+                        ),
+                      ],
+                    );
+                  }
                 ),
               ],
             ),
