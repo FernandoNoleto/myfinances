8428222efe83fbb2d64f8dbaa9fe4144f51be5f3
fernandonoleto <fernando.barroso.noleto@gmail.com>
2023-11-20 17:57:17 -0300

    feat(home_page): Renderiza um CircularChart como exemplo

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:myfinances/pages/new_expense_page.dart';
-// import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 // import 'package:syncfusion_flutter_charts/sparkcharts.dart';
 
 
@@ -24,6 +24,21 @@ class HomePageState extends StatefulWidget {
 }
 
 class _HomePageStateState extends State<HomePageState> {
+  late List<_ChartData> data;
+  late TooltipBehavior _tooltip;
+
+  @override
+  void initState() {
+    data = [
+      _ChartData('David', 25),
+      _ChartData('Steve', 38),
+      _ChartData('Jack', 34),
+      _ChartData('Others', 52)
+    ];
+    _tooltip = TooltipBehavior(enable: true);
+    super.initState();
+  }
+
   List getListExpenses(){
     return [];
   }
@@ -43,8 +58,35 @@ class _HomePageStateState extends State<HomePageState> {
                 Navigator.push(context, CupertinoPageRoute(builder: (context) => const NewExpensePage()));
             }),
           ),
+          SliverFillRemaining(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SfCircularChart(
+                  tooltipBehavior: _tooltip,
+                  series: <CircularSeries<_ChartData, String>>[
+                    DoughnutSeries<_ChartData, String>(
+                      dataSource: data,
+                      explode: true,
+                      xValueMapper: (_ChartData data, _) => data.x, //key: string
+                      yValueMapper: (_ChartData data, _) => data.y, //value: int
+                      name: 'Gold'
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          )
         ],
-      )
+      ),
     );
   }
 }
+
+class _ChartData {
+  _ChartData(this.x, this.y);
+
+  final String x;
+  final double y;
+}
+
